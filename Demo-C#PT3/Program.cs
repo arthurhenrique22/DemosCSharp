54a71490e246a3bebcb6116651e49a17f3f7298e
arthurhenrique22 <77027468+arthurhenrique22@users.noreply.github.com>
2022-02-11 17:22:39 -0300

    DemoC#SobreRefsEStruct

---
+++ Demo-C#PT3/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Arthur";
+p1.Idade = 17;
+p1.EnderecoPessoa = new Pessoa.Endereco()
+{
+    Logradouro= "Rua teste",
+    Numero = 300,
+    CEP = "00000",
+    Cidade = "Pernambuco",
+};
+
+WriteLine("Fim");
